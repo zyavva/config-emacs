80d20ba5aec50552fdc4c4f15f2ba3532ecb9416
Wojtek Wlodarczyk <Wojciech.Wlodarczyk@sabre.com>
2017-06-27 12:36:53 +0200

    configure whitspace-mode & add expand-region & add fold-this

--- init.el
+++ init.el
@@ -6,9 +6,7 @@
  '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
-    (json-mode
-     ;; requires 'markdown' command-line tool installed in system
-     markdown-mode))))
+    (fold-this expand-region intero json-mode markdown-mode))))
 
 (custom-set-faces)
 
@@ -21,11 +19,19 @@
 
 (global-linum-mode t)
 (global-whitespace-mode)
-(setq whitespace-style '(face spaces trailing space-before-tab newline indentation
- empty space-after-tab space-mark tab-mark newline-mark))
+(setq whitespace-style
+ '(face spaces trailing space-before-tab newline empty space-after-tab space-mark tab-mark newline-mark))
 
 ;; spelling - requires 'aspell' program installed
 (setq ispell-program-name "aspell")
 (require 'ispell)
+
+(require 'expand-region)
+(global-set-key (kbd "C-M-w") 'er/expand-region)
+
+(require 'fold-this)
+(global-set-key (kbd "C-c C--") 'fold-this-all)
+(global-set-key (kbd "C-c C-+") 'fold-this)
+(global-set-key (kbd "C-c M-+") 'fold-this-unfold-all)
 
 (server-start)
