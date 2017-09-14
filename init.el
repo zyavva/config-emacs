d9ab40ba88c4908ee3afbfd99064a341e1dadca6
Wojtek Wlodarczyk <Wojciech.Wlodarczyk@sabre.com>
2017-09-14 09:24:05 +0200

    CUA - remove keeping region after copy

--- init.el
+++ init.el
@@ -63,6 +63,6 @@
 (cua-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
-(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+;;(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
 
 (server-start)
