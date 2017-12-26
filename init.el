032855745264bb53524b26d26d378079c8105a10
Wojtek Wlodarczyk <wmwlodarczyk@gmail.com>
2017-12-26 20:24:03 +0100

    refactor after installation on t-60

--- init.el
+++ init.el
@@ -37,12 +37,18 @@
   (add-hook 'markdown-mode-hook 'flyspell-mode))
 
 (custom-set-variables
- '(package-selected-packages
-   '(expand-region
-     json-mode
-     markdown-mode)))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (expand-region json-mode markdown-mode))))
 
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; configuration
 
