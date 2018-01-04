b9e1f791188bcb3facf32e63d461ae7722ff44aa
Wojtek Wlodarczyk <wmwlodarczyk@gmail.com>
2018-01-04 23:55:17 +0100

    Revert "fix for markdown mode problem in dark theme"
    This reverts commit c27ce8dbe2e209d411644e080d6029462a174dec.

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-code-face ((t (:inherit fixed-pitch))))) ;; fix for markdown mode problem in dark theme
+ )
 
 ;; configuration
 
