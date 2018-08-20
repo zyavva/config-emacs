bcc6e8facbc19b479224bdda4343524f11ebb0cb
Wojtek Wlodarczyk <wojciech.wlodarczyk@sabre.com>
2018-08-20 15:12:44 +0200

    switch to reverse-video & refactor

--- init.el
+++ init.el
@@ -1,20 +1,20 @@
-; connection proxy for office environment
+;;; connection proxy for office environment
+
 (when (equal system-name "DCT9R1N2")
   (setq url-proxy-services
-	'(("no_proxy" . "^(localhost|127\.0\.0\.1)")
-	  ("http" . "127.0.0.1:3128")
-	  ("https" . "127.0.0.1:3128"))))
+    '(("no_proxy" . "^(localhost|127\.0\.0\.1)")
+      ("http" . "127.0.0.1:3128")
+      ("https" . "127.0.0.1:3128"))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; package management
+;;; package management
 
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-; activate all the packages
+;; activate all the packages
 (package-initialize)
 
-; fetch the list of packages available
+;; fetch the list of packages available
 (unless package-archive-contents
   (package-refresh-contents)
   (package-install 'use-package)) ; and install one package to rule them all
@@ -28,14 +28,17 @@
 (use-package expand-region
   :ensure t
   :pin melpa-stable
-  :bind (("M-@" . er/expand-region)
-	 ("C-S-w" . er/contract-region)))
+  :bind (
+     ("M-@" . er/expand-region)
+     ("C-S-w" . er/contract-region)))
 
 (use-package markdown-mode ; requires 'markdown' command for preview
   :ensure t
   :pin melpa-stable
   :init
   (add-hook 'markdown-mode-hook 'flyspell-mode))
+
+;;; configuration
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -49,10 +52,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; configuration
+ '(whitespace-space ((t (:foreground "darkgray"))))) ; override space background colour in whitespace mode
 
 ;; miscellaneous global variables
 (setq
@@ -68,7 +68,7 @@
 ;; starting emacs client full screen
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-; lose the UI
+;; lose the UI
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
@@ -82,20 +82,13 @@
 (setq ispell-program-name "aspell")
 (require 'ispell)
 
-;; Common User Access key bindings
-;(cua-mode t)
-;(transient-mark-mode 1) ; No region when it is not highlighted
-;(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-; custom keybindings for CUA mode
-;(global-set-key (kbd "C->") 'cua-scroll-up)
-;(global-set-key (kbd "C-<") 'cua-scroll-down)
+;; use Common User Access features without using its key bindings
+(cua-mode t)
+(setq cua-enable-cua-keys nil) ;; disable the overriding of standard Emacs binding by CUA mode
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 
 ;; custom keybindings
-; invoke M-x without the Alt key
-;(global-set-key "\C-x\C-m" 'execute-extended-command)
+;(global-set-key "\C-x\C-m" 'execute-extended-command) ; invoke M-x without the Alt key
 ;(global-set-key "\C-w" 'backward-kill-word)
 
-; set theme
-(load-file "~/.emacs.d/themes/default-black-theme.el")
-
-(server-start)
+; (server-start)
