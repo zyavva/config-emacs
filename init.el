18e1b67b93850346d7ee6a7b20f745bd602064d4
Wojciech Wlodarczyk <w.wlodarczyk@ocado.com>
2018-10-11 10:18:24 +0200

    remove dired hacks

--- init.el
+++ init.el
@@ -103,40 +103,7 @@
 
 ;; dired
 (require 'dired )
-; kill current dired buffer when changing director
-(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
-(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))
 (setq dired-dwim-target t) ; copy from one dired dir to the next dired dir shown in a split window
-; (add-hook 'dired-load-hook (function (lambda () (load "dired-x")))) ; hints with commands on '!'
-(defun xah-open-in-external-app ()
-  "Open the current file or dired marked files in external app.
-The app is chosen from your OS's preference.
-URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
-Version 2016-10-15"
-  (interactive)
-  (let* (
-         ($file-list
-          (if (string-equal major-mode "dired-mode")
-              (dired-get-marked-files)
-            (list (buffer-file-name))))
-         ($do-it-p (if (<= (length $file-list) 5)
-                       t
-                     (y-or-n-p "Open more than 5 files? "))))
-    (when $do-it-p
-      (cond
-       ((string-equal system-type "windows-nt")
-        (mapc
-         (lambda ($fpath)
-           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" $fpath t t))) $file-list))
-       ((string-equal system-type "darwin")
-        (mapc
-         (lambda ($fpath)
-           (shell-command
-            (concat "open " (shell-quote-argument $fpath))))  $file-list))
-       ((string-equal system-type "gnu/linux")
-        (mapc
-         (lambda ($fpath) (let ((process-connection-type nil))
-                            (start-process "" nil "xdg-open" $fpath))) $file-list))))))
 
 ; (server-start)
 (put 'dired-find-alternate-file 'disabled nil)
